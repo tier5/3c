84743484235dec2a90f2eb8b2c2ba45ff0fd225a
Tier5 LLC <github@tier5.us>
2017-12-14 23:09:42 +0530

    Widget Socket v0.11

--- app.js
+++ app.js
@@ -3,141 +3,126 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var axios = require('axios');
 
-
 /** Chat App */
-io.on('connection', function(socket) {
-    console.log('User connected');
+io.on('connection', function (socket) {
+  console.log('User connected');
 
-    // Event emited when a client connects
-    socket.on('client-connected', function(data) {
-        console.log('Node: Client Connected: ', data);
-        // Api call to create room for client and agents
-        axios.post('http://3c.local/api/v1/add-chat-user', data)
-            .then(function (res) {
-                if(res.data.status) {
-                    var resp  = res.data.response;
-                    console.log('Emiting Event from to vue: clientAddedToRoom')
-                    socket.emit('clientAddedToRoom', resp);
-                    socket.join(resp.room_number);
-                    io.sockets.in(resp.room_number).emit('connectedToRoom', "We are connecting you to an agent");
-                    console.log(resp.name + 'is joined  to room : ', resp.room_number);
-                    io.sockets.in(resp.room_number).emit('updateRoom', resp);
-                    sendRooms();
-                } else {
-                    console.log(res)
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    });
-
-    // Event emited when an agent gets connected
-    socket.on('get-added-rooms', function() {
-        console.log('Node: Get Added Rooms');
-        sendRooms();
-    });
-
-    // Event emited by agents when they want to get added to some rooms
-    socket.on('add-agent-to-rooms', function(rooms) {
-    //    if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
-        console.log('Node: Add Agent To Rooms ', rooms);
-        for (var i = 0, len = rooms.length; i < len; i++) {
-            if(!(rooms[i].name in socket.rooms)) {
-                socket.join(rooms[i].name);
-                socket.emit('agent-added-to-room', rooms[i]);
-                console.log('Agent Added To Room ', rooms[i].name);
-                if (rooms[i].status == 1) {
-                    io.sockets.in(rooms[i].name).emit('msg-of-acceptance', "You have to 'ACCEPT' or 'REJECT' this Message!");
-                }
-            }
-        }
-    });
-
-    // API call to get all the agent list and the list of rooms they are assigned to with status.
-    var sendRooms = function() {
-        console.log('In Send Rooms Function');
-
-    //    TODO: API calls to get data of all agent_id and rooms they are assigned to with status
-        axios.get('http://3c.local/api/v1/get-agents')
-            .then(function (res) {
-                if(res.data.status) {
-                    console.log('Node : New Rooms Added ', res.data.response);
-                    io.sockets.emit('new-rooms-added', res.data.response);
-                } else {
-                    console.log(res);
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    }
-
-    socket.on('msg', function(data) {
-        // Send message to everyone in that particular room
-
-        console.log('message sent');
-        // console.log(data);
-        /**api call to add message to the database */
-        axios.post('http://3c.local/api/v1/add-messages', data)
-            .then(function (response) {
-                if(response.data.status) {
-                    // Send message to everyone in that particular room
-                    console.log(response.data.response);
-                    io.sockets.in(data.roomNo)
-                        .emit('newmsg',data);
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-        // /**api call to get message to the database */
-        // axios.post('http://3c.local/api/v1/get-messages', { roomNumber : data.roomNo} )
-        //     .then(function (response) {
-        //         if(response.data.status) {
-        //             // Send message to everyone in that particular room
-        //             console.log(response.data.response);
-        //         }
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
-    });
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    
-    socket.on('setUsername', function(data) {
-        console.log(data);
-        
-        if(users.indexOf(data) > -1) {
-            socket.emit('userExists', data + ' Username is taken! Try some other username.');
+  /** Event emitted when a client connects */
+  socket.on('client-connected', function (data) {
+    console.log('Node: Client Connected: ', data);
+    /** Api call to create room for client and agents */
+    axios.post('http://3c.local/api/v1/add-chat-user', data)
+      .then(function (res) {
+        if (res.data.status) {
+          var resp = res.data.response;
+          console.log('Emitting Event from to vue: clientAddedToRoom');
+          socket.emit('clientAddedToRoom', resp);
+          socket.join(resp.room_number);
+          io.sockets.in(resp.room_number).emit('connectedToRoom', 'We are connecting you to an agent');
+          console.log(resp.name + 'is joined  to room : ', resp.room_number);
+          io.sockets.in(resp.room_number).emit('updateRoom', resp);
+          sendRooms();
         } else {
-            users.push(data);
-            socket.emit('userSet', {username: data});
-            
-            // Increase room no if 2 clients are present in a room.
-            if (io.sockets.adapter.rooms["room-"+roomno] && io.sockets.adapter.rooms["room-"+roomno].length > 1) {
-                roomno++;
-            }
-            socket.join("room-"+roomno);
-
-            //Send this event to everyone in the room.
-            io.sockets.in("room-"+roomno)
-                .emit('connectToRoom', { roomNo: roomno });
+          console.log(res);
         }
-    });
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  });
 
-    /** on socket disconnection */
-    socket.on('disconnect', function()  {
-        console.log(socket.id,'disconnected');
-        
-        io.sockets.in(socket.id).emit('disconnect');
-        socket.disconnect();
-    });  
+  /** Event emitted when an agent gets connected */
+  socket.on('get-added-rooms', function () {
+    console.log('Node: Get Added Rooms');
+    sendRooms();
+  });
+
+  /** Event emitted by agents when they want to get added to some rooms */
+  socket.on('add-agent-to-rooms', function (rooms) {
+    console.log('Node: Add Agent To Rooms ', rooms);
+    for (var i = 0, len = rooms.length; i < len; i++) {
+      if (!(rooms[i].name in socket.rooms)) {
+        socket.join(rooms[i].name);
+        socket.emit('agent-added-to-room', rooms[i]);
+        console.log('Agent Added To Room ', rooms[i].name);
+        if (rooms[i].status == 1) {
+          io.sockets.in(rooms[i].name).emit('msg-of-acceptance', 'You have to \'ACCEPT\' or \'REJECT\' this Message!');
+        }
+      }
+    }
+  });
+
+  /** API call to get all the agent list and the list of rooms they are assigned to with status. */
+  var sendRooms = function () {
+    console.log('In Send Rooms Function');
+
+    /** API calls to get data of all agent_id and rooms they are assigned to with status */
+    axios.get('http://3c.local/api/v1/get-agents')
+      .then(function (res) {
+        if (res.data.status) {
+          console.log('Node : New Rooms Added ', res.data.response);
+          io.sockets.emit('new-rooms-added', res.data.response);
+        } else {
+          console.log(res);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  /** Send message to everyone in a particular room */
+  socket.on('msg', function (data) {
+
+    console.log('message sent');
+
+    /** Api call to add message to the database */
+    axios.post('http://3c.local/api/v1/add-messages', data)
+      .then(function (res) {
+        if (res.data.status) {
+          /** Send message to everyone in that particular room */
+          console.log(res.data.response);
+          io.sockets.in(data.roomNo).emit('newmsg', data);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // /**api call to get message to the database */
+    // axios.post('http://3c.local/api/v1/get-messages', { roomNumber : data.roomNo} )
+    //     .then(function (response) {
+    //         if(response.data.status) {
+    //             // Send message to everyone in that particular room
+    //             console.log(response.data.response);
+    //         }
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+  });
+
+  /** Agent Accepts msg */
+  socket.on('agent-accepts-msg', function(data) {
+
+    /** Api call to accept msg for agent */
+    axios.post('http://3c.local/api/v1/accept-chat-request', data)
+      .then(function (res) {
+        if (res.data.status) {
+          console.log('After Accept');
+          console.log(res.data);
+          socket.emit('which-agent-accepted', res,data.response)
+        } else {
+          console.log(res);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
 
 });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
