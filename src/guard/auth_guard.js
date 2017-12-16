a006ccdcac0cbb5b59eab781121ee7d31a7578b2
tier5 <work@tier5.us>
2017-12-16 19:36:31 +0530

    Socket implementation v0.13

--- src/guard/auth_guard.js
+++ src/guard/auth_guard.js
@@ -1,9 +1,6 @@
 
 export default (to, from, next) => {
-  console.log(to);
-  console.log(from);
-  console.log(next);
-  console.log(localStorage.getItem('client'));
+
   if (from.path === '/' && from.name ==='ChatRequest' && localStorage.getItem('client')) {
     next();
   } else {
