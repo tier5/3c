b6981459ee0ab57a5f77a19b7b800c9e164af605
tier5 <work@tier5.us>
2017-12-29 13:21:48 +0530

    backend released v5.4

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var io = require('socket.io')(http);
 var axios = require('axios');
 var bodyParser = require('body-parser');
 
-var API_URL = 'http://3c.local/api/v1/';
-//var API_URL = 'http://138.197.215.68/api/v1/';
+//var API_URL = 'http://3c.local/api/v1/';
+var API_URL = 'http://138.197.215.68/api/v1/';
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}));
