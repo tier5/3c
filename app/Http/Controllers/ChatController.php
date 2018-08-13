31e58bf4d9f1b910221f21eca4cb1be3e80beb2f
Tier5 LLC <work@tier5.us>
2018-08-13 22:21:09 +0530

    v71.0 released

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -110,14 +110,14 @@ class ChatController extends Controller
                             $this->saveOtherChat( $fromNumber, $widgetUuid, $messageBody, $type, $direction, $userId );
                         } elseif($checkMessageTrack->status == 5){
                             Log::info('2 ===> check message status 5');
-                            //$this->createSmsTemplate($fromNumber, $widgetUuid);
+                            $this->createSmsTemplate($fromNumber, $widgetUuid);
                             $updateMessageTrack= MessageTrack::where('widget_id',$widgetUuid)
                                 ->where('from_phone_number',$fromNumber)->update([ 'status' => 6]);
                             $updateMessageCache = MessageCache::where('from_phone_number',$fromNumber)
                                 ->where('widget_uuid',$widgetUuid)->first();
                             $updateMessageCache->status = 6;
                             $updateMessageCache->update();
-                            $this->checkResolvedMessageCache($messageBody,$fromNumber,$widgetUuid,$updateMessageCache->id);
+                            //$this->checkResolvedMessageCache($messageBody,$fromNumber,$widgetUuid,$updateMessageCache->id);
                         } else {
                             Log::info('2 ===> ');
                             //$this->createSmsTemplate($fromNumber, $widgetUuid);
