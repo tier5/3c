3aeaa3d717e4d3db829c35794d75b1762a4292eb
Tier5 LLC <work@tier5.us>
2018-09-10 19:34:19 +0530

    v143.0 released

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var axios = require('axios');
 var bodyParser = require('body-parser');
 
-//var API_URL = 'http://3c.local/api/v1/';
+//var API_URL = 'http://localhost:8000/api/v1/';
 var API_URL = 'http://138.197.215.68/api/v1/';
 
 // parse application/x-www-form-urlencoded
