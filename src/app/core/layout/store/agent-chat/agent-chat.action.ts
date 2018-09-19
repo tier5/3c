f29f404fda6a9ffbe18dce57c4422f41baec4e20
Tier5 LLC <github@tier5.us>
2018-09-19 20:17:43 +0530

    admin close chat history v1.0

--- src/app/core/layout/store/agent-chat/agent-chat.action.ts
+++ src/app/core/layout/store/agent-chat/agent-chat.action.ts
@@ -2,6 +2,8 @@ import { Action } from '@ngrx/store';
 
 export const GET_AGENT_LIVE_CHAT_ATTEMPT = 'GET_AGENT_LIVE_CHAT_ATTEMPT';
 export const GET_AGENT_LIVE_CHAT_SUCCESS = 'GET_AGENT_LIVE_CHAT_SUCCESS';
+export const GET_AGENT_CLOSE_CHAT_ATTEMPT = 'GET_AGENT_CLOSE_CHAT_ATTEMPT';
+export const GET_AGENT_CLOSE_CHAT_SUCCESS = 'GET_AGENT_LIVE_CHAT_SUCCESS';
 
 export class GetAgentLiveChatAttempt implements Action {
   readonly type = GET_AGENT_LIVE_CHAT_ATTEMPT;
@@ -12,4 +14,13 @@ export class GetAgentLiveChatSuccess implements Action {
   constructor(public payload: any) { }
 }
 
-export type AgentChatActions = GetAgentLiveChatAttempt | GetAgentLiveChatSuccess;
+export class GetAgentCloseChatSuccess implements Action {
+    readonly type = GET_AGENT_CLOSE_CHAT_SUCCESS;
+    constructor(public payload: any) { }
+}
+
+export class GetAgentCloseChatAttempt implements Action {
+    readonly type = GET_AGENT_CLOSE_CHAT_ATTEMPT;
+}
+
+export type AgentChatActions = GetAgentLiveChatAttempt | GetAgentLiveChatSuccess | GetAgentCloseChatAttempt | GetAgentCloseChatSuccess;
