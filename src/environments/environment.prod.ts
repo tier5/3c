0ef3264300e341a8c409263b34292d89a6e879d7
Tier5 LLC <work@tier5.us>
2018-09-26 15:43:01 +0530

    Tmsms-frontend V2.0 released

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://178.128.187.125/api/v1/',
-  SOCKET_URL: 'http://178.128.187.125:3000'
+  API_BASE_URL: 'http://138.197.215.68/api/v1/',
+  SOCKET_URL: 'http://138.197.215.68:3000'
 };
