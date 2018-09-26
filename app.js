faeab88da2fa663c4333ff8325491a38abcca895
Tier5 LLC <work@tier5.us>
2018-09-26 15:44:23 +0530

    Tmsms-master V2.0 released

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var axios = require('axios');
 var bodyParser = require('body-parser');
 
 //var API_URL = 'http://localhost:8000/api/v1/';
-var API_URL = 'http://178.128.187.125/api/v1/';
+var API_URL = 'http://138.197.215.68/api/v1/';
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}));
--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1504,7 +1504,7 @@ class ChatController extends Controller
                 $toNumber = "";
             }
             $smsBody = $getAgent ? $getAgent->first_name . ',' : '';
-            $smsBody .= ' You have ' . $getAgent->pendingChatCount->count() . ' chat requests at http://greys.telemojo.net/pending ';
+            $smsBody .= ' You have ' . $getAgent->pendingChatCount->count() . ' chat requests at http://sms.telemojo.net/pending ';
             //$smsBody = "link to visit the page in the website http://sms.telemojo.com/chat/ongoing (demo api url)";
             /** Try to send sms */
             \Log::info('$smsBody-->' . $smsBody . '$agentPhoneNumber-->' . $agentPhoneNumber . '$toNumber-->' . $toNumber);
