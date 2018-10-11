ef3739b7f2f096933f391fccd3c50b9b5c4d5738
Tier5 LLC <work@tier5.us>
2018-10-11 20:49:55 +0530

    Fixed Transfer chat rejected issue

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1677,27 +1677,55 @@ class ChatController extends Controller
         $status = 3;    //reject Scenario
 
         $updateMessageAgentTrack = MessageAgentTrack::where('id', $messageAgentTrackId)->update(['status' => $status]);
-        $countRejectedChat = MessageAgentTrack::where('message_id', $messageId)->where('chat_room_id', $chatRoomId)->where('status', $status)->count();
-        //$updateMessageTrack      = MessageTrack::where('message_id',$messageId)->update(['agent_id'=>$agentId,'status'=>$status]);
-        $checkMessageForwardCounter = MessageForwardCounter::where('id', $messageForwardCountId)->first();
-        if ($checkMessageForwardCounter->agent_count == $countRejectedChat) {
 
-            $checkMessageForwardCounter->count_init = ($checkMessageForwardCounter->count_init + 1);
-            $checkMessageForwardCounter->update();
-            $updateMessageTrack = MessageTrack::where('message_id', $messageId)->update(['status' => 1]); //message track status will be 1 for again inicating the other messages
-            $updateMessageAgentTrack = MessageAgentTrack::where('message_forward_counter_id', $checkMessageForwardCounter->id)->update(['status' => 1]); //update this table for reinicate the chat request to agents
-            $this->inicateRejectNotificationToAgents($checkMessageForwardCounter->id, $widgetUuid);
+        $transfer = AgentTransferHistory::where('other_agent_ids',$agentId)->where('chat_room_id',$chatRoomId)->where('status','0')->first();
+        if ($transfer) {
+            $checkMessageTrack = MessageTrack::where('message_id', $transfer->message_id)->first();
+            if ($checkMessageTrack) {
+                $updateMessageTrack = MessageTrack::where('message_id', $transfer->message_id)
+                    ->update([
+                        'status' => 2,
+                        'agent_id' => $transfer->transfer_from_agent_id
+                    ]);
 
-            //call to node API
-            $url = url('/') . ':3000/send-rooms';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
-            curl_close($ch);
+                $deleteMessageTract = MessageAgentTrack::where('chat_room_id',$checkMessageTrack->chat_room_id)
+                    ->where('widget_id', $checkMessageTrack->widget_id)->delete();
 
+                $messageAgentTrack = new MessageAgentTrack();
+                $messageAgentTrack->agent_id = $transfer->transfer_from_agent_id;
+                $messageAgentTrack->message_id = $checkMessageTrack->message_id;
+                $messageAgentTrack->chat_room_id = $checkMessageTrack->chat_room_id;
+                $messageAgentTrack->widget_id = $checkMessageTrack->widget_id;
+                $messageAgentTrack->message_forward_counter_id = 1;
+                $messageAgentTrack->status = 2;
+                $messageAgentTrack->save();
+
+                $transfer->delete();
+
+                $body = 'Transferred chat is rejected by the agent so it has been reinstated to you.';
+
+                $time = date("Y-m-d H:i:s");
+                $url = url('/') . ':3000/mobile-chat';
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS,
+                    "messageBody=$body&direction=4&user=System&chatRoomId=$checkMessageTrack->chat_room_id&time=$time");
+
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                curl_close($ch);
+
+                //call to node API
+                $url = url('/') . ':3000/send-rooms';
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                curl_close($ch);
+            }
             $response = ['agentId' => $agentId, 'chatRoomId' => $chatRoomId, 'status' => $status];
             return Response::json(array(
                 'status' => true,
@@ -1706,27 +1734,59 @@ class ChatController extends Controller
                 'response' => $response,
                 'message' => 'Agent and chatroom with Reject Status !'
             ));
-
         } else {
-            //call to node API
-            $url = url('/') . ':3000/send-rooms';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
-            curl_close($ch);
+            $countRejectedChat = MessageAgentTrack::where('message_id', $messageId)->where('chat_room_id', $chatRoomId)->where('status', $status)->count();
+            //$updateMessageTrack      = MessageTrack::where('message_id',$messageId)->update(['agent_id'=>$agentId,'status'=>$status]);
+            $checkMessageForwardCounter = MessageForwardCounter::where('id', $messageForwardCountId)->first();
+            if ($checkMessageForwardCounter->agent_count == $countRejectedChat) {
 
-            $response = ['agentId' => $agentId, 'chatRoomId' => $chatRoomId, 'status' => $status];
-            return Response::json(array(
-                'status' => true,
-                'code' => 200,
-                'error' => false,
-                'response' => $response,
-                'message' => 'Agent and chatroom with Reject Status !'
-            ));
+                $checkMessageForwardCounter->count_init = ($checkMessageForwardCounter->count_init + 1);
+                $checkMessageForwardCounter->update();
+                $updateMessageTrack = MessageTrack::where('message_id', $messageId)->update(['status' => 1]); //message track status will be 1 for again inicating the other messages
+                $updateMessageAgentTrack = MessageAgentTrack::where('message_forward_counter_id', $checkMessageForwardCounter->id)->update(['status' => 1]); //update this table for reinicate the chat request to agents
+                $this->inicateRejectNotificationToAgents($checkMessageForwardCounter->id, $widgetUuid);
+
+                //call to node API
+                $url = url('/') . ':3000/send-rooms';
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                curl_close($ch);
+
+                $response = ['agentId' => $agentId, 'chatRoomId' => $chatRoomId, 'status' => $status];
+                return Response::json(array(
+                    'status' => true,
+                    'code' => 200,
+                    'error' => false,
+                    'response' => $response,
+                    'message' => 'Agent and chatroom with Reject Status !'
+                ));
+
+            } else {
+                //call to node API
+                $url = url('/') . ':3000/send-rooms';
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                curl_close($ch);
+
+                $response = ['agentId' => $agentId, 'chatRoomId' => $chatRoomId, 'status' => $status];
+                return Response::json(array(
+                    'status' => true,
+                    'code' => 200,
+                    'error' => false,
+                    'response' => $response,
+                    'message' => 'Agent and chatroom with Reject Status !'
+                ));
+            }
         }
+
     }
 
 
