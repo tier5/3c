0cf04cb03c761ecf03795aab34144da737bf8e7f
Tier5 LLC <work@tier5.us>
2018-10-12 16:58:28 +0530

    Fix runtime pending chat issue

--- app.js
+++ app.js
@@ -15,11 +15,8 @@ app.use(bodyParser.json());
 
 /** Chat App */
 io.on('connection', function (socket) {
-    console.log('User connected');
-
     /** Rest api */
     app.post('/send-rooms', function (req, res) {
-        console.log('In post');
         sendRooms();
         res.status(200).json({
             status: true
@@ -28,8 +25,6 @@ io.on('connection', function (socket) {
 
     /** Rest api */
     app.post('/mobile-chat', function (req, res) {
-        console.log('In mobile chat post edited');
-        console.log(req.body);
         newMsg(req.body);
         res.status(200).json({
             status: true,
@@ -38,7 +33,6 @@ io.on('connection', function (socket) {
     });
 
     app.post('/agent-notification', function (req, res) {
-        console.log('agent notification');
         io.sockets.in(data.chatRoomId).emit('agentNotification','Transferred chat is not accepted by anyone so it has been reinstated to you.');
         res.status(200).json({
             status: true
@@ -47,22 +41,17 @@ io.on('connection', function (socket) {
     
     /** Event emitted when a client connects */
     socket.on('client-connected', function (data) {
-        console.log('Node: Client Connected: ', data);
         /** Api call to create room for client and agents */
         axios.post(API_URL + 'web-chat', data)
             .then(function (res) {
-                console.log("response ");
                 if (res.data.status) {
                     var resp = res.data.response;
-                    console.log('Emitting Event from to vue: clientAddedToRoom');
                     socket.emit('clientAddedToRoom', resp);
                     socket.join(resp.chatRoomId);
                     io.sockets.in(resp.chatRoomId).emit('connectedToRoom', 'We are connecting you to an agent');
-                    console.log(resp.name + 'is joined  to room : ', resp.chatRoomId);
                     io.sockets.in(resp.chatRoomId).emit('updateRoom', resp);
                     sendRooms();
                 } else {
-                    console.log(res);
                     socket.emit('clientNotAddedToRoom');
                 }
             })
@@ -73,18 +62,15 @@ io.on('connection', function (socket) {
 
     /** Event emitted when an agent gets connected */
     socket.on('get-added-rooms', function () {
-        console.log('Node: Get Added Rooms');
         sendRooms();
     });
 
     /** Event emitted by agents when they want to get added to some rooms */
     socket.on('add-agent-to-rooms', function (rooms) {
-        console.log('Node: Add Agent To Rooms ');
         for (var i = 0, len = rooms.length; i < len; i++) {
             if (!(rooms[i].name in socket.rooms)) {
                 socket.join(rooms[i].name);
                 socket.emit('agent-added-to-room', rooms[i]);
-                console.log('Agent Added To Room ', rooms[i].name);
                 if (rooms[i].status == 1) {
                     io.sockets.in(rooms[i].name).emit('msg-of-acceptance', 'You have to \'ACCEPT\' or \'REJECT\' this Message!');
                 }
@@ -94,13 +80,10 @@ io.on('connection', function (socket) {
 
     /** API call to get all the agent list and the list of rooms they are assigned to with status. */
     var sendRooms = function () {
-        console.log('In Send Rooms Function');
-
         /** API calls to get data of all agent_id and rooms they are assigned to with status */
         axios.get(API_URL + 'all-agents-chatrooms')
             .then(function (res) {
                 if (res.data.status) {
-                    console.log('Node : New Rooms Added ', res.data.response);
                     io.sockets.emit('new-rooms-added', res.data.response);
                 } else {
                     console.log(res);
@@ -116,10 +99,8 @@ io.on('connection', function (socket) {
         /**api call to add message to the database */
         axios.post(API_URL + 'web-chat-message', data)
             .then(function (response) {
-                console.log(response.data);
                 if(response.data.status) {
                     // Send message to everyone in that particular room
-                    console.log(response.data.response);
                     io.sockets.in(data.chatRoomId).emit('newmsg',response.data.response);
                 }
             })
@@ -130,20 +111,15 @@ io.on('connection', function (socket) {
 
     /** Send message to everyone in a particular room */
     socket.on('msg', function (data) {
-        console.log('message sent');
-        console.log(data);
         newMsg(data);
     });
 
     /** Agent accepts message */
     socket.on('agent-performed-some-action', function (data) {
-
-        console.log('Actions ', data);
         /** Api call to accept msg for agent */
         axios.post(API_URL + 'agent-chat-action', data)
             .then(function (res) {
                 if (res.data.status) {
-                    console.log('After Action: ', res.data);
                     if (data.status == 2) {
                         io.sockets.in(res.data.response.chatRoomId).emit('which-agent-accepted', res.data.response);
                     } else if (data.status == 3) {
@@ -155,6 +131,7 @@ io.on('connection', function (socket) {
                         io.sockets.in(res.data.response.chatRoomId).emit('which-agent-resolved', res.data.response);
                         io.sockets.in(res.data.response.chatRoomId).emit('clientChatResolved', 'Thank You For Connecting With Us!');
                     }
+                    sendRooms();
                 } else {
                     console.log(res);
                 }
@@ -176,7 +153,6 @@ io.on('connection', function (socket) {
 
     /** on socket disconnection */
     socket.on('disconnect', function()  {
-        console.log(socket.id,'disconnected');
         // io.sockets.in(data.chatRoomId).emit('client-disconnected', data);
         io.sockets.in(socket.id).emit('disconnect');
         socket.disconnect();
